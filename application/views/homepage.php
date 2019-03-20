c4faaadc24e0cdfd459836392f88bc22908276c6
Tyrone Battung <tyronebattung@gmail.com>
2019-03-20 23:20:08 +0800

    First comming by Battung

---
+++ application/views/homepage.php
@@ -0,0 +1,10 @@
+<html>
+<head>
+<title>BATTUNG</title>
+<link href="assets/css/battungstyle.css" rel="stylesheet" type="text/css">
+</head>
+<body>
+	<p> Test Battung </p>
+	<script src="assets/js/battung.js"></script>
+	</body>
+	</html>
