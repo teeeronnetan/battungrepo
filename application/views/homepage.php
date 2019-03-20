74a4e7dfb24702fa1cf92cbe016c193954c49a55
Arch Jay Jabla <bliltinked@gmail.com>
2019-03-20 23:23:42 +0800

    I changed my classmate's repo

--- application/views/homepage.php
+++ application/views/homepage.php
@@ -4,7 +4,7 @@
 <link href="assets/css/battungstyle.css" rel="stylesheet" type="text/css">
 </head>
 <body>
-	<p> Test Battung </p>
+	<p> Gwapo si Jay </p>
 	<script src="assets/js/battung.js"></script>
 	</body>
 	</html>
